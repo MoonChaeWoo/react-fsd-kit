91bba859d8fe52323b5afeaaabc1bacfd36185d8
moonchaewoo <tyche0421@naver.com>
2025-09-18 03:11:05 +0900

    Tailwind 적용

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
@@ -9,7 +10,10 @@ export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, process.cwd(), '');
 
     return {
-        plugins: [react()],
+        plugins: [
+            react(),
+            tailwindcss()
+        ],
         server: {
             port: Number(env.VITE_PORT) || (mode === 'development' && 5173)
         },
